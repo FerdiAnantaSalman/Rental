8ed6455565286e29d23bd8e693da1be840c07886
Ferdi Ananta Salman <ferdiananta51@gmail.com>
2022-10-23 21:00:40 +0700

    ip

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -48,7 +48,7 @@
                 <div class="sb-sidenav-menu">
                     <div class="nav">
                         <div class="sb-sidenav-menu-heading">Core</div>
-                        <a class="nav-link" href="index.html">
+                        <a class="nav-link" href="http://127.0.0.1:8000">
                             <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                             Dashboard
                         </a>
